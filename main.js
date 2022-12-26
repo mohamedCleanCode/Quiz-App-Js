7eabeab7ac522f37c480eec1dd6b5f1bc1d399eb
mohamed <m635379@gmail.com>
2022-12-26 17:16:18 +0200

    add event on start button and add timer function

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// Start button
+let startBtn = document.querySelector(".start button");
+startBtn.addEventListener("click", (e) => {
+  e.target.parentElement.remove();
+  setInterval(() => {
+    timer();
+  }, 1000);
+});
+
+// Timer function
+const timer = () => {
+  let count = document.querySelector(".countdown .count");
+  count.innerHTML = count.innerHTML - 1;
+};
 // fetch("./db.json")
 //   .then((res) => res.json())
 //   .then((data) => console.log(data));
