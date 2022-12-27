b43895b5cea12f12da6a44cd6ddb27dd0f55a9c3
mohamed <m635379@gmail.com>
2022-12-27 20:17:41 +0200

    add clickingAnswer function

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
+// Main varibles
 let infoAboutLeft = document.querySelector(".info-about-left");
 let queContainer = document.querySelector(".question");
 let answersContainer = document.querySelector(".answers");
+let questions = [];
 let num = 1;
-let length;
 
 fetch("./db.json")
   .then((res) => res.json())
   .then((data) => {
-    length = data.length;
-    updateInfoAboutLeft(data.length);
-    setDataInDom(data);
+    questions = data;
   });
 
 // Start button
-// let startBtn = document.querySelector(".start button");
-// startBtn.addEventListener("click", (e) => {
-//   e.target.parentElement.remove();
-//   setInterval(() => {
-//     timer();
-//   }, 1000);
-// });
+let startBtn = document.querySelector(".start button");
+startBtn.addEventListener("click", (e) => {
+  e.target.parentElement.remove();
+  setInterval(() => {
+    timer();
+  }, 1000);
+
+  setDataInDom(questions);
+  updateInfoAboutLeft(num, questions.length);
+  clickingAnswer();
+});
 
 // Timer function
 const timer = () => {
@@ -28,37 +31,59 @@ const timer = () => {
 };
 
 // Set data in DOM
-const setDataInDom = (arr) => {
+const setDataInDom = (questions) => {
   let oldQue = [];
-  let randomQue = arr[Math.floor(Math.random() * arr.length)];
+  let randomQue = questions[Math.floor(Math.random() * questions.length)];
   oldQue.push(randomQue.id);
-  if (oldQue.includes()) {
-    setDataInDom(arr);
-  } else {
-    queContainer.innerHTML = `
+  queContainer.innerHTML = `
                     <span class="question-number">${num}.</span>
                 <p class="question-title">${randomQue.question}...</p>
     `;
-    num++;
-    for (let i = 0; i < 4; i++) {
-      let div = document.createElement("div");
-      div.className = "answer";
-      let input = document.createElement("input");
-      let label = document.createElement("label");
-      input.type = "radio";
-      input.id = `${randomQue.incorrect_answers[i]}`;
-      input.name = "answer";
-      input.dataset.answer = `${randomQue.incorrect_answers[i]}`;
-      label.htmlFor = `${randomQue.incorrect_answers[i]}`;
-      label.innerHTML = `${randomQue.incorrect_answers[i]}`;
-      div.appendChild(input);
-      div.appendChild(label);
-      answersContainer.appendChild(div);
-    }
+  for (let i = 0; i < 4; i++) {
+    let div = document.createElement("div");
+    div.className = "answer";
+    let input = document.createElement("input");
+    let label = document.createElement("label");
+    input.type = "radio";
+    input.id = `${randomQue.incorrect_answers[i]}`;
+    input.name = "answer";
+    input.dataset.answer = `${randomQue.correct_answer}`;
+    label.htmlFor = `${randomQue.incorrect_answers[i]}`;
+    label.innerHTML = `${randomQue.incorrect_answers[i]}`;
+    div.appendChild(input);
+    div.appendChild(label);
+    answersContainer.appendChild(div);
   }
 };
 
 // Update info-about-left
-const updateInfoAboutLeft = (num1) => {
-  infoAboutLeft.innerHTML = `<p>${num1} Of ${num1} Questions</p>`;
+const updateInfoAboutLeft = (num1, num2) => {
+  infoAboutLeft.innerHTML = `<p>${num1} Of ${num2} Questions</p>`;
+  num++;
+};
+
+// Cliking on answer function
+const clickingAnswer = () => {
+  let answers = document.querySelectorAll(".answer label");
+  answers.forEach((answer) => {
+    answer.addEventListener("click", (e) => {
+      let input = e.target.previousElementSibling.dataset.answer;
+      if (e.target.innerHTML === input) {
+        e.target.parentElement.classList.add("good");
+        answers.forEach((ele) => {
+          if (!ele.parentElement.classList.contains("good")) {
+            ele.parentElement.classList.add("bad");
+          }
+        });
+      } else {
+        answers.forEach((ele) => {
+          if (ele.innerHTML === input) {
+            ele.parentElement.classList.add("good");
+          } else {
+            ele.parentElement.classList.add("bad");
+          }
+        });
+      }
+    });
+  });
 };
